91229f9c674d03020794f7394214744f4844ad74
nakarinh14 <nakarinh14@gmail.com>
2021-02-23 20:12:42 +0700

    Add middleware to verify Firebase user session with encoded IDToken

---
+++ express-backend/src/api/middlewares/verifyUserSession.js
@@ -0,0 +1,18 @@
+import admin from '../../config/firebaseConfig'
+
+
+export const authVerification = async (req, res, next) => {
+    try {
+        // idToken comes from the client app
+        const decodedToken = await admin.auth().verifyIdToken(req.body.idToken);
+        // Attach to res object to be process by next middlewares
+        res.locals.uid = decodedToken.uid;
+        next()
+    }
+    catch (e){
+        res.status(401).send({
+            error: e
+        })
+    }
+}
+
