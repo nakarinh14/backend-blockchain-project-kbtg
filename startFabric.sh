13ff9c9558ad4a39c6539e705408e1a9fe170a95
nakarinh14 <nakarinh14@gmail.com>
2021-03-03 17:54:33 +0700

    Update bash script for starting order and fabric-samples directory

--- startFabric.sh
+++ startFabric.sh
@@ -1,15 +1,19 @@
-# Start node backend
-npm start --prefix express-backend/
+# Fetch fabric samples and binary
+# curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.3.0 1.4.9
+export PATH=${PWD}/fabric-samples/bin:$PATH
 
 CC_SRC_LANGUAGE="javascript"
-CC_SRC_PATH="../chaincode/"
+CC_SRC_PATH="../../chaincode/"
 
 # Clean out any old identites in the wallets
 rm -rf javascript/wallet/*
 
 # launch network
-pushd ../test-network
+pushd ./fabric-samples/test-network
 ./network.sh down
 ./network.sh up createChannel -ca -s couchdb
-./network.sh deployCC -ccn ktw -ccv 1 -cci initLedger -ccl ${CC_SRC_LANGUAGE} -ccp ${CC_SRC_PATH}
+./network.sh deployCC -ccn ktw-coin -ccv 1 -ccl ${CC_SRC_LANGUAGE} -ccp ${CC_SRC_PATH}
 popd
+
+# Start node backend
+npm start --prefix express-backend/
